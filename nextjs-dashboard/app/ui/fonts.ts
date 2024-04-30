9544756d0da439abd68db2e67d1d6f03893d3fa5
Irina Spasova <irina.spasova.81@gmail.com>
2024-04-30 16:08:48 +0300

    Hero section

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana  } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const open_sans = Open_Sans({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
